bab03d691a745cf1d1fc2a02b0754a1545baa483
Shunsuke Miyazaki <miyazaki@future-d.co.jp>
2017-11-25 14:33:39 +0900

    remove code supple & fix index typo

--- javascript/result-checker.js
+++ javascript/result-checker.js
@@ -1,11 +1,11 @@
 function result_checker(i){
-  var answer = document.getElementById('answer');
-  var elem = document.getElementById('code-result-'+i);
 
-  if (elem.value != answer.value) {
-    elem.style.backgroundColor = '#F9444D';
-  }
-  else {
-    elem.style.backgroundColor = '';
-  }
+    var answer = document.getElementById('answer');
+    var elem = document.getElementById('code-result-'+i);
+
+    if (elem.value != answer.value) {
+        elem.style.backgroundColor = '#F9444D';
+    } else {
+        elem.style.backgroundColor = '';
+    }
 }
