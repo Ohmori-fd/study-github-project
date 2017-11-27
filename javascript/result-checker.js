926bf94b93ab60c2a96a98c06eccd77248df7bd1
Shunsuke 'bep' Miyazaki <beplocks661@gmail.com>
2017-11-28 00:59:42 +0900

    reset js & fix etc

--- javascript/result-checker.js
+++ javascript/result-checker.js
@@ -3,9 +3,14 @@ function result_checker(i){
     var answer = document.getElementById('answer');
     var elem = document.getElementById('code-result-'+i);
 
+    //reset
+    elem.style.backgroundColor = '';
+
     if (elem.value != answer.value) {
+        //error
         elem.style.backgroundColor = '#F9444D';
     } else {
-        elem.style.backgroundColor = '';
+        //good
+        elem.style.backgroundColor = '#81D8D0';
     }
 }
